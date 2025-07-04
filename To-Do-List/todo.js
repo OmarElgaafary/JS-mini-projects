c78e1f78806b0d91c211d23c6a4d3279d164e1d5
Omar Adly <omarelgaafaryy@gmail.com>
2025-07-05 01:16:38 +0300

    Completed To Do List

--- To-Do-List/todo.js
+++ To-Do-List/todo.js
@@ -11,30 +11,27 @@ function displaySearch() {
     const name = searchInput.value;
     const dueDate = dueDateInput.value;
 
+    if (!name || !dueDate)
+        return;
+
     array.push({
-        name,
-        dueDate
+        name: name,
+        dueDate: dueDate
     });
     updateToDos(array);
     searchInput.value = '';
 
 }
 
-const enterDown = (event) => {
-    if (event.key == 'Enter')
-        displaySearch();
-}
-
 const updateToDos = (array) => {
     toDoItems.innerHTML = '';
     for (let i = 0; i < array.length; i++) {
         if (array[i])
-            toDoItems.innerHTML += `<p>${array[i]}</p><button onclick="array.splice(${i}, 1); displaySearch();">Delete</button>`;
+            toDoItems.innerHTML += `<div class"inner-to-do-items"><p class="js-paragraph">${array[i].name} ${array[i].dueDate}</p><button class="js-button" onclick="array.splice(${i}, 1); updateToDos(array);">Delete</button></div>`;
     }
 }
 
-const deleteToDo = (id) => {
-    const idElement1 = document.getElementById(`${id1}`);
-    if (idElement1)
-        idElement1.remove();
+const enterDown = (event) => {
+    if (event.key == 'Enter')
+        displaySearch();
 }
