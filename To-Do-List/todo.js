8f2ad3aaa27265538202873614adf73ec9575338
Omar Adly <omarelgaafaryy@gmail.com>
2025-07-04 23:48:46 +0300

    Added ToDoList

--- To-Do-List/todo.js
+++ To-Do-List/todo.js
@@ -1,11 +1,40 @@
 const searchInput = document.querySelector('.to-do-search');
+const dueDateInput = document.querySelector('.js-due-date');
+const toDoItems = document.querySelector('.to-do-items');
+const toDoDelete = document.querySelector('.to-do-delete');
+
 
 let array = [];
 let string;
 
 function displaySearch() {
-    let current = searchInput.value;
-    array.push(current);
-    console.log(array);
+    const name = searchInput.value;
+    const dueDate = dueDateInput.value;
+
+    array.push({
+        name,
+        dueDate
+    });
+    updateToDos(array);
+    searchInput.value = '';
+
 }
 
+const enterDown = (event) => {
+    if (event.key == 'Enter')
+        displaySearch();
+}
+
+const updateToDos = (array) => {
+    toDoItems.innerHTML = '';
+    for (let i = 0; i < array.length; i++) {
+        if (array[i])
+            toDoItems.innerHTML += `<p>${array[i]}</p><button onclick="array.splice(${i}, 1); displaySearch();">Delete</button>`;
+    }
+}
+
+const deleteToDo = (id) => {
+    const idElement1 = document.getElementById(`${id1}`);
+    if (idElement1)
+        idElement1.remove();
+}
